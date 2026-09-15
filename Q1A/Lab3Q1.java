77a0c072552f5ca946355e32ffaf260570139357
IT24200716 <It24200716@my.sliit.lk>
2026-09-15 15:05:19 +0530

    Lab 3Q1A

---
+++ Q1A/Lab3Q1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lab3Q1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter price of 1kg of rice: ");
+        double price = input.nextDouble();
+
+        System.out.print("Enter number of kilograms: ");
+        double kg = input.nextDouble();
+
+        double amount = price * kg;
+
+        System.out.println("Amount to pay = " + amount);
+    }
+}
